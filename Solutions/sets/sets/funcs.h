d3247b64ed804fe321ddf864eaa102d9748a87d8
mariaPetrova18912 <maria18912@yandex.ru>
2016-04-14 22:40:19 +0300

    hhh

--- Solutions/sets/sets/funcs.h
+++ Solutions/sets/sets/funcs.h
@@ -2,26 +2,26 @@
 #include <time.h>
 #include <cstdlib>
 
-void countino(sets *set, int &co, int n, int m) {
+void countino(sets *set, int *co, int n, int m) {
   bool f = false;
   int x;
   int y;
   srand(time(0));
   int z;
-  while (f == false && co < n * m) {
+  while (f == false && (*co) < n * m) {
     do {
         x = rand() % n;
         y = rand() % m;
        } while ((*set).arr[x + (y + 1)*n] != -1);
-    co++;
+    (*co)++;
     z = x + (y + 1)*n;
     (*set).add(z);
-    if (x > 0)
+    if (x != 0)
       if ((*set).arr[z - 1] != -1)
         (*set).un(z, z - 1);
       if ((*set).arr[z - n] != -1)
         (*set).un(z, z - n);
-      if (x < n - 1)
+      if (x != n - 1)
         if ((*set).arr[z + 1] != -1)
           (*set).un(z, z + 1);
       if ((*set).arr[z + n] != -1)
--- Solutions/sets/sets/main.cpp
+++ Solutions/sets/sets/main.cpp
@@ -11,7 +11,7 @@ void main() {
     for (int i = 0; i < numb; i++) {
       preparation(&set, n, m);
       count = 0;
-      countino(&set, count, n, m);
+      countino(&set, &count, n, m);
       res += count * 1.0 / n / m;
     }
     std::cout <<"percentage = " << (res / numb)
